b5547280ff7d688f8f9da0524b780ae332bb86c5
Eliyan <eliyan.dochev@epitech.eu>
2022-09-30 16:41:54 +0200

    [CODING STYLE] missed a space

--- lib/my/str_cat.c
+++ lib/my/str_cat.c
@@ -9,8 +9,8 @@
 
 char *str_join(char const *str_one, char const *str_two)
 {
-    int length_str_one = str_one != NULL ? my_strlen(str_one): 0;
-    int length_str_two = str_two != NULL ? my_strlen(str_two): 0;
+    int length_str_one = str_one != NULL ? my_strlen(str_one) : 0;
+    int length_str_two = str_two != NULL ? my_strlen(str_two) : 0;
     char *new_str = str_one != NULL && str_two != NULL ?
     malloc(length_str_one + length_str_two + 1) : NULL;
 
